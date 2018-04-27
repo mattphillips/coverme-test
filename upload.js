7b834a14d1811fb76eb1889fd79113a497be2d4a
Matt Phillips <matt@mattphillips.io>
2018-04-27 16:02:45 +0100

    Add ci

---
+++ upload.js
@@ -0,0 +1,36 @@
+import fs from 'fs';
+import fetch from 'node-fetch';
+import FormData from 'form-data';
+import path from 'path';
+import shell from 'shelljs';
+
+const p = path.resolve(__dirname, 'coverage/lcov-report');
+
+const repoName = process.argv[3];
+const sha = shell.exec('git rev-parse HEAD').substring(0, 7);
+
+const getAllFiles = dir =>
+  fs.readdirSync(dir).reduce((files, file) => {
+    const name = path.join(dir, file);
+    const isDirectory = fs.statSync(name).isDirectory();
+    return isDirectory ? [...files, ...getAllFiles(name)] : [...files, name];
+  }, []);
+
+const files = getAllFiles(p).map(file => ({ relative: file.replace(p, ''), absolute: file }));
+
+const formData = new FormData();
+files.forEach(file => {
+  formData.append(file.relative, fs.createReadStream(file.absolute));
+});
+
+formData.append('repo', repoName);
+formData.append('sha', sha);
+
+fetch('https://coverme-test.now.sh/coverage', {
+  method: 'POST',
+  body: formData,
+})
+  .then(res => res.text())
+  // .then(text => console.log(text));
+
+
